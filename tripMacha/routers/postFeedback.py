778bbe480adc423bd227c14931893aee78624616
Chirantan Banik <banikchirantan16@gmail.com>
2024-01-17 14:34:54 +0530

    Fixed the pydantic module error

--- tripMacha/routers/postFeedback.py
+++ tripMacha/routers/postFeedback.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from config.database import feedback_collection
 from models.SaveModel import FeedbackCollection
 from bson import ObjectId
-import pydantic
+import pydantic.v1
 
 feedback_api_router=APIRouter()
 
@@ -12,7 +12,7 @@ def encode_object_id(obj):
         return str(obj)
     raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
 # Add the custom encoder to pydantic's ENCODERS_BY_TYPE dictionary
-pydantic.json.ENCODERS_BY_TYPE[ObjectId] = encode_object_id
+pydantic.v1.json.ENCODERS_BY_TYPE[ObjectId] = encode_object_id
 @feedback_api_router.post("/feedback")
 def createFeedback(feedback: FeedbackCollection):
     data = feedback.dict()
