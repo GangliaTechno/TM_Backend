24b0b597fc915e2dabfe0025478d522740796366
Blurr99 <vnbaxla0107@gmail.com>
2024-01-20 10:32:41 +0530

    Added check for whether user exists or not in the database

--- tripMacha/routers/firebase.py
+++ tripMacha/routers/firebase.py
@@ -3,11 +3,19 @@ from firebase_admin import auth
 
 firebase_api_router= APIRouter()
 
+NoUser = {
+    'uid': None,
+    'email': None,
+    'display_name': None,
+    'photo': 'https://logowik.com/content/uploads/images/cat8600.jpg',
+    'phone_no': 9999999999
+}
+
+
 @firebase_api_router.get("/userinfo/{firebase_id}")
 def get_user(firebase_id: str):
-    #try:
-        print("its executing")
-        user = auth.get_user(firebase_id)
+    user = auth.get_user(firebase_id)
+    if (user):
         json_data = {
             'uid': user.uid,
             'email': user.email,
@@ -15,3 +23,5 @@ def get_user(firebase_id: str):
             'photo':user.photo_url,
             'phone_no':user.phone_number}
         return json_data
+    else:
+        return NoUser
